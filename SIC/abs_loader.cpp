e90d590e272855b5060ac8c1cd49e1c6cc3a0e08
anisham197 <anisha.m.197@gmail.com>
2017-03-22 00:20:08 +0530

    Hi

--- SIC/abs_loader.cpp
+++ SIC/abs_loader.cpp
@@ -4,58 +4,46 @@
 
 using namespace std;
 
-class p1 {
+void parseInput()
+{
 
-	public:
+	fstream input_file, output_file;
+	string line, addr;
+	char hex_loc[4];
+	int loc;
 
-	void parseInput()
+	// Open files
+	input_file.open("objcode.txt", ios::in );
+	output_file.open("abs_load_output.txt", ios::out);
+
+	// Read input file till end
+	while ( getline(input_file, line) )
 	{
+		// Checks for header record
+		if(line[0] == 'H')
+			continue;
 
-		fstream input_file, output_file;
-		string line, addr;
-		char cbuff[100], hex_loc[4];
-    		int loc;
+		// Checks for end record
+		if ( line[0] == 'E')
+			break;
 
-		// Open files
-		input_file.open("objcode.txt", ios::in );
-		output_file.open("abs_load_output.txt", ios::out | ios::app);
+		// Retrieves starting address from text record, converts to int
+		addr = line.substr(3,4);
+		loc = strtol(addr.c_str(), NULL, 16);
 
-		// Read input file till end
-		while ( getline(input_file, line) )
+		// Iterates through object code bytes of text record
+		for ( int i = 9; line[i] != '\0'; i = i+2 )
 		{
-			// Clears cbuff from previous input
-			memset(&cbuff, 0, sizeof(cbuff));
+  			sprintf(hex_loc, "%X", loc);
+			// Loads hex location and one byte to file
+  			output_file << hex_loc <<"\t"<<line[i]<<line[i+1]<<endl;
+  			loc = loc + 1;
+		}
 
-			// Copies input line to cbuff(char array)
-			strcpy(cbuff, line.c_str());
-
-      			// Checks for header record
-      			if(cbuff[0] == 'H')
-        			continue;
-
-			// Checks for end record
-      			if ( cbuff[0] == 'E')
-        			break;
-
-			// Retrieves starting address from text record, converts to int
-      			addr = line.substr(3,4);
-      			loc = strtol(addr.c_str(), NULL, 16);
-
-			// Iterates through object code bytes of text record
-      			for ( int i = 9; cbuff[i] != '\0'; i = i+2 )
-      			{
-          			sprintf(hex_loc, "%X", loc);
-				// Loads hex location and one byte to file
-          			output_file << hex_loc <<"\t"<<cbuff[i]<<cbuff[i+1]<<endl;
-          			loc = loc + 1;
-      			}
-
-    		}
-  	}
-};
+	}
+}
 
 int main ()
 {
-	p1 obj;
-	obj.parseInput();
+	parseInput();
 }
--- SIC/pass1.cpp
+++ SIC/pass1.cpp
@@ -6,165 +6,157 @@
 
 using namespace std;
 
-class p1 {
+map <string, string> optab;
+map <string, string>::iterator iter;
 
-	map <string, string> optab;
-	map <string, string>::iterator iter;
+// Reading optab.txt and creating hashmap of symbol and opcode
+void readOptab() 
+{
 
-	public:
+	fstream file;
+	string input1, input2;
 
-	// Reading optab.txt and creating hashmap of symbol and opcode
-	void readOptab() 
+	file.open("optab3.txt", ios::in );
+	while (  file >> input1 >> input2 ) 
 	{
+		optab.insert ( make_pair ( input1 , input2 ) ) ;
+	}
+}
 
-		fstream file;
-		string input1, input2;
+void parseInput() 
+{
 
-		file.open("optab3.txt", ios::in );
-		while (  file >> input1 >> input2 ) 
+	int locctr = 0, lineno = 0;
+	fstream input_file, sym_file, output_file;
+	string line;
+	char hex_loc[5];
+	char *str, *token;
+
+	// Open files
+	input_file.open("input3.txt", ios::in );
+	output_file.open("locctr.txt", ios::out);
+	sym_file.open("symtab.txt", ios::out);
+
+	// Read input file till end
+	while ( getline(input_file, line) )
+	{
+		
+		char tokens[3][20]={0,0,0};
+
+		lineno++;
+
+		// Checks for comment line
+		if(line[0] == '.')
 		{
-			optab.insert ( make_pair ( input1 , input2 ) ) ;
+			output_file << line << endl;
+			continue;
+		}
+
+		// Copies to str (char pointer), tokenizes into LABEL, MNEMONIC, OPERAND
+		str = strdup(line.c_str());
+
+		int i = 0;
+		// Splits by "\t" delimiter
+		while((token = strsep( &str, "\t")) && i < 3) 
+		{
+				strcpy(tokens[i],token);
+				i++;
+		}
+
+		//Check if Mnemonic is START
+		if (strcmp(tokens[1],"START") == 0)
+		{
+			// Stores initial location
+			locctr = strtol(tokens[2], NULL, 16);
+			output_file <<"\t" << line << endl;
+			continue;
+		}
+
+		else if (strcmp(tokens[1],"END") == 0)
+		{
+				// Converts location to hex
+				sprintf(hex_loc, "%X", locctr);
+				sym_file << tokens[1] <<"\t" << hex_loc << endl;
+				output_file << hex_loc << "\t\t" << tokens[1]<< endl;
+				continue;
+		}
+
+		// Checks if MNEMONIC exits in optab
+		else if ( optab.find(tokens[1]) == optab.end())
+		{
+			cout << "\nERROR in line " << lineno << endl;
+			break;
+		}
+
+		// Writes line to output file with location
+		sprintf(hex_loc, "%X", locctr);
+		output_file << hex_loc<< "\t"<< tokens[0] <<"\t"<< tokens[1]<< "\t"<< tokens[2]<< endl;
+
+		// If symbol in LABEL field, write to symtab with hex location
+		if(strlen(tokens[0])!=0)
+		{
+			sym_file << tokens[0] <<"\t" << hex_loc << endl;
+		}
+
+		// Obtain (key, value) from map for MNEMONIC
+		iter = optab.find(tokens[1]);
+
+		// Handle location counter update
+		if( iter->second == "*" )
+		{
+			// Checks error where no label defined for WORD, BYTE, RESW, RESB				
+			if ( strlen(tokens[0]) == 0 )
+			{
+				cout << "ERROR, No variable defined in line " << lineno << endl;
+				break;			
+			}
+
+			//Handle BYTE
+			if(strcmp(tokens[1],"BYTE") == 0)
+			{
+				if(tokens[2][0] == 'C')
+				{
+					locctr +=  strlen(tokens[2]) - 3;
+					// -3 for C , ', '
+				}
+				else if(tokens[2][0] == 'X')
+				{
+					int hexdigits = strlen(tokens[2])- 3;
+					locctr += ( hexdigits % 2 == 0 ? hexdigits/2 :(hexdigits+1)/2 );
+				}
+				else
+				{
+					cout << "\nERROR in line " << lineno << endl;
+					break;
+				}
+			}
+			else if(strcmp(tokens[1],"RESB")==0)
+			{
+				locctr += strtol(tokens[2], NULL, 10);
+			}
+			else if(strcmp(tokens[1],"RESW")==0)
+			{
+				locctr += strtol(tokens[2], NULL, 10)*3;
+			}
+			else if(strcmp(tokens[1],"WORD")==0)
+			{
+				locctr += 3;
+			}
+		}
+
+		// Increments by 3 if normal instruction
+		else {
+				locctr += 3;
 		}
 	}
 
-	void parseInput() 
-	{
-
-		int locctr = 0, lineno = 0;
-		fstream input_file, sym_file, output_file;
-		string line;
-		char cbuff[100], tokens[3][20], hex_loc[4];
-		char *str, *token;
-
-		// Open files
-		input_file.open("input3.txt", ios::in );
-		output_file.open("locctr.txt", ios::out | ios::app);
-		sym_file.open("symtab.txt", ios::out | ios::app);
-
-		// Read input file till end
-		while ( getline(input_file, line) )
-		{
-			lineno++;
-
-			// Copies input line to cbuff(char array)
-			strcpy(cbuff, line.c_str());
-
-			// Checks for comment line
-			if(cbuff[0] == '.')
-			{
-				output_file << line << endl;
-				continue;
-			}
-
-			// Clears all the tokens
-			for ( int i = 0; i < 3; i++)
-				memset(&tokens[i], 0, sizeof(tokens[i]));
-
-			// Copies to str (char pointer), tokenizes into LABEL, MNEMONIC, OPERAND
-			str = strdup (cbuff);
-			int i = 0;
-			// Splits by "\t" delimiter
-			while((token = strsep(&str, "\t")) && i < 3) 
-			{
-					strcpy(tokens[i],token);
-					i++;
-			}
-
-			//Check if Mnemonic is START
-			if (strcmp(tokens[1],"START") == 0)
-			{
-				// Stores initial location
-				locctr = strtol(tokens[2], NULL, 16);
-				output_file <<"\t" << line << endl;
-				continue;
-			}
-
-			else if (strcmp(tokens[1],"END") == 0)
-			{
-					// Converts location to hex
-					sprintf(hex_loc, "%X", locctr);
-					sym_file << tokens[1] <<"\t" << hex_loc << endl;
-					output_file << hex_loc << "\t\t" << tokens[1]<< endl;
-					continue;
-			}
-
-			// Checks if MNEMONIC exits in optab
-			else if ( optab.find(tokens[1]) == optab.end())
-			{
-				cout << "\nERROR in line " << lineno << endl;
-				break;
-			}
-
-			// Writes line to output file with location
-			sprintf(hex_loc, "%X", locctr);
-			output_file << hex_loc<< "\t"<< tokens[0] <<"\t"<< tokens[1]<< "\t"<< tokens[2]<< endl;
-
-			// If symbol in LABEL field, write to symtab with hex location
-			if(strlen(tokens[0])!=0)
-			{
-				sym_file << tokens[0] <<"\t" << hex_loc << endl;
-			}
-
-			// Obtain (key, value) from map for MNEMONIC
-			iter = optab.find(tokens[1]);
-
-			// Handle location counter update
-			if((strlen(tokens[0])!=0) && (strcmp( iter->second.c_str(),"*") == 0))
-			{
-				//Handle BYTE
-				if(strcmp(tokens[1],"BYTE") == 0)
-				{
-					if(tokens[2][0] == 'C')
-					{
-						locctr +=  strlen(tokens[2]) - 3;
-						// -3 for C , ', '
-					}
-					else if(tokens[2][0] == 'X')
-					{
-						int hexdigits = strlen(tokens[2])- 3;
-						locctr += ( hexdigits % 2 == 0 ? hexdigits/2 :(hexdigits+1)/2 );
-					}
-					else
-					{
-						cout << "\nERROR in line " << lineno << endl;
-						break;
-					}
-				}
-				else if(strcmp(tokens[1],"RESB")==0)
-				{
-					locctr += strtol(tokens[2], NULL, 10);
-				}
-				else if(strcmp(tokens[1],"RESW")==0)
-				{
-					locctr += strtol(tokens[2], NULL, 10)*3;
-				}
-				else if(strcmp(tokens[1],"WORD")==0)
-				{
-					locctr += 3;
-				}
-			}
-
-			// Checks error where no label defined for WORD, BYTE, RESW, RESB
-			else if ( (strlen(tokens[0])==0) && (strcmp( iter->second.c_str(),"*") == 0))
-			{
-				cout << "ERROR in line " << lineno << endl;
-				cout << "No variable defined " << endl;
-				break;
-			}
-			// Increments by 3 if normal instruction
-			else {
-					locctr += 3;
-			}
-		}
-		input_file.close();
-		sym_file.close();
-		output_file.close();
-	}
-};
+	input_file.close();
+	sym_file.close();
+	output_file.close();
+}
 
 int main () 
 {
-	p1 obj;
-	obj.readOptab();
-	obj.parseInput();
+	readOptab();
+	parseInput();
 }
